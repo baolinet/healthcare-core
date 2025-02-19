f03e7fbbfad8346a3ef18ee4792ce6f4cff387fc
baolinet <bao.li.net@hotmail.com>
2025-02-20 00:22:48 +0800

    add

---
+++ src/test/java/com/healthcare/healthcare_core/HealthcareCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.healthcare_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
